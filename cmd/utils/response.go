16a125e76ef4425238e4b4a2df89722199f5d1ee
milijan-mosic <milijan.mosic@gmail.com>
2026-01-19 21:47:18 +0100

    Improved func for sending early exit response

--- cmd/utils/response.go
+++ cmd/utils/response.go
@@ -1,10 +1,17 @@
 package utils
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 func SetJSONResponse(w http.ResponseWriter, httpCode int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(httpCode)
+
+	if message != "" {
+		_ = json.NewEncoder(w).Encode(map[string]string{
+			"message": message,
+		})
+	}
 }

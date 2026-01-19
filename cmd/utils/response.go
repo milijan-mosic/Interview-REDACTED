5a0823c028372001a3c715504fd834b6c7323ccf
milijan-mosic <milijan.mosic@gmail.com>
2026-01-19 20:40:33 +0100

    Added empty files for future use (handlers)

--- cmd/utils/response.go
+++ cmd/utils/response.go
@@ -1,15 +1,10 @@
 package utils
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-func SetJSONResponse(message string, httpCode int, w http.ResponseWriter) {
+func SetJSONResponse(w http.ResponseWriter, httpCode int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(httpCode)
-
-	_ = json.NewEncoder(w).Encode(map[string]string{
-		"message": message,
-	})
 }
---
+++ internal/httpapi/debug/stats.go
@@ -0,0 +1,11 @@
+package httpapi
+
+import (
+	"interview/cmd/utils"
+	"net/http"
+)
+
+func PartsStatsHandler(w http.ResponseWriter, r *http.Request) {
+	message := "Hello world!"
+	utils.SetJSONResponse(w, http.StatusOK, message)
+}

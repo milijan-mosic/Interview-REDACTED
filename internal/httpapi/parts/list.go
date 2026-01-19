b490fc2bdd2ca2e182e74b53fe6e6e1eff4e2bec
milijan-mosic <milijan.mosic@gmail.com>
2026-01-19 22:11:22 +0100

    Developed the "list products" route

--- internal/httpapi/parts/list.go
+++ internal/httpapi/parts/list.go
@@ -1,11 +1,55 @@
 package parts
 
 import (
+	"encoding/json"
 	"interview/cmd/utils"
+	"interview/internal/store"
+	"log"
 	"net/http"
 )
 
 func ListPartsHandler(w http.ResponseWriter, r *http.Request) {
-	message := "Hello world!"
-	utils.SetJSONResponse(w, http.StatusOK, message)
+	if r.Method != http.MethodGet {
+		utils.SetJSONResponse(w, http.StatusMethodNotAllowed, "Invalid HTTP method")
+		return
+	}
+
+	rows, err := store.DB.Query("SELECT * FROM parts")
+	if err != nil {
+		log.Println(err)
+		utils.SetJSONResponse(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	defer rows.Close()
+
+	parts := []store.Part{}
+
+	for rows.Next() {
+		var p store.Part
+		err := rows.Scan(
+			&p.ID,
+			&p.Name,
+			&p.Status,
+			&p.Supplier,
+			&p.Material,
+			&p.Weight,
+			&p.Critical,
+			&p.Updated,
+		)
+		if err != nil {
+			log.Println(err)
+			utils.SetJSONResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		parts = append(parts, p)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Println(err)
+		utils.SetJSONResponse(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.SetJSONResponse(w, http.StatusOK, "")
+	_ = json.NewEncoder(w).Encode(parts)
 }
